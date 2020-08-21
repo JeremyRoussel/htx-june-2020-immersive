f496a5786cda17b3318cfc4fab242e104b07d7c0
Jeremy Roussel <jeremy.roussel13@gmail.com>
2020-08-21 16:26:05 -0500

    Aug 21 Claswork

---
+++ week12/4-Thursday/labs-class-ex/styling/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div style={{fontSize:"3rem", backgroundColor:"slateblue", color:"white", textAlign:"center"  }} > Hello, World!</div>
+    </>
+  )
+}
+
+export default App
